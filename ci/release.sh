3b24ebb60af1dfec24a3f96d886bd8ac5110bbd1
Corbs <corbsmartin@pivotal.io>
2018-11-13 16:53:24 -0600

    resource-release-output

--- ci/release.sh
+++ ci/release.sh
@@ -4,6 +4,6 @@ echo "Packaging Release for version $VERSION"
 echo `cat resource-build-output/target/classes/META-INF/build-info.properties`
 BUILD_NAME=`cat resource-build-output/target/classes/META-INF/build-info.properties | grep build.name | cut -d'=' -f2`
 
-cp resource-build-output/target/classes/META-INF/build-info.properties resource-build-output/release-body
-echo "${BUILD_NAME}" > resource-build-output/release-name
-echo "${VERSION}" > resource-build-output/release-tag
+cp resource-build-output/target/classes/META-INF/build-info.properties resource-release-output/release-body
+echo "${BUILD_NAME}" > resource-release-output/release-name
+echo "${VERSION}" > resource-release-output/release-tag
