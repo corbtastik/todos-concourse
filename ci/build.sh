c0c85e36cc7308947f3804c39af4a225f56b5632
Corbs <corbsmartin@pivotal.io>
2018-11-13 10:44:33 -0600

    setting maven build version

--- ci/build.sh
+++ ci/build.sh
@@ -3,7 +3,8 @@ export MAVEN_OPTS="-Xms2g -Xmx2g"
 export VERSION=`cat resource-semver/version`
 echo "Building version $VERSION"
 cd resource-source-code
-  ./mvnw clean package -Dversion=${VERSION} -Dmaven.test.skip=true
+  ./mvnw versions:set -DnewVersion=${VERSION}
+  ./mvnw clean package -Dmaven.test.skip=true
 cd ..
 
 cp resource-source-code/target/todos-webflux-${VERSION}.jar  resource-build-output/.
