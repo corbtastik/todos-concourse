54a739845c3aeefda3663bfd01b26480b889a5a1
Corbs <corbsmartin@pivotal.io>
2018-11-14 14:21:02 -0600

    enabled all pipelines

--- create_pipeline.sh
+++ create_pipeline.sh
@@ -5,3 +5,21 @@ fly sp -t todos-cicd -c pipeline-release.yml -p todos-webflux-release -l creds.y
   -v resource_version_uri=git@github.com:corbtastik/todos-version.git \
   -v resource_release_owner=corbtastik \
   -v resource_release_repository=todos-webflux
+
+fly sp -t todos-cicd -c pipeline-release.yml -p todos-api-release -l creds.yml \
+  -v resource_source_code_uri=git@github.com:corbtastik/todos-api.git \
+  -v resource_version_uri=git@github.com:corbtastik/todos-version.git \
+  -v resource_release_owner=corbtastik \
+  -v resource_release_repository=todos-api
+
+fly sp -t todos-cicd -c pipeline-release.yml -p todos-cloud-gateway-release -l creds.yml \
+  -v resource_source_code_uri=git@github.com:corbtastik/todos-cloud-gateway.git \
+  -v resource_version_uri=git@github.com:corbtastik/todos-version.git \
+  -v resource_release_owner=corbtastik \
+  -v resource_release_repository=todos-cloud-gateway
+
+fly sp -t todos-cicd -c pipeline-release.yml -p todos-ui -l creds.yml \
+  -v resource_source_code_uri=git@github.com:corbtastik/todos-ui.git \
+  -v resource_version_uri=git@github.com:corbtastik/todos-version.git \
+  -v resource_release_owner=corbtastik \
+  -v resource_release_repository=todos-ui  
