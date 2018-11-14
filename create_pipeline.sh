4a4ed2189769de66fedafd21fedb5dbe72ab1dd7
Corbs <corbsmartin@pivotal.io>
2018-11-14 13:16:52 -0600

    doh

--- create_pipeline.sh
+++ create_pipeline.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-fly sp -t todos-cicd -c pipeline-atomic.yml -p todos-webflux-release -l creds.yml \
+fly sp -t todos-cicd -c pipeline-release.yml -p todos-webflux-release -l creds.yml \
   -v resource_source_code_uri=git@github.com:corbtastik/todos-webflux.git \
   -v resource_version_uri=git@github.com:corbtastik/todos-version.git \
   -v resource_release_owner=corbtastik \
