c09afb101e337c8199d3b85c74a762b8da948d61
Corbs <corbsmartin@pivotal.io>
2018-11-16 11:38:30 -0600

    deploy plumbing

--- ci/deploy.sh
+++ ci/deploy.sh
@@ -1,14 +1,9 @@
-#!/bin/sh
-# unzip release to get built application bits
-# cross reference to inventoried app manifests
-# output config with inventoried manifest
-cat resource-release/body
-cat resource-release/commit_sha
-cat resource-release/tag
-cat resource-release/version
-# echo "hi" > resource-release-output/hi
-# ls -al
-# cat resource-release-output/hi
-# echo "bye"
+#!/bin/bash
+BUILD_TIME=`cat resource-release/body | grep -w "build.time" | cut -d'=' -f2`
+BUILD_NAME=`cat resource-release/body | grep -w "build.name" | cut -d'=' -f2`
+BUILD_ARTIFACT=`cat resource-release/body | grep -w "build.artifact" | cut -d'=' -f2`
+BUILD_GROUP=`cat resource-release/body | grep -w "build.group" | cut -d'=' -f2`
+BUILD_VERSION=`cat resource-release/body | grep -w "build.version" | cut -d'=' -f2`
 
-  
+sed -e "s/\${artifact}/resource-release\\/${BUILD_ARTIFACT}-${BUILD_VERSION}.jar/" \
+    resource-concourse/ci/manifest/${BUILD_ARTIFACT}-manifest.yml > resource-release-output/manifest.yml
--- pipeline_deploy.sh
+++ pipeline_deploy.sh
@@ -5,3 +5,6 @@ fly sp -t todos-cicd -c pipeline-deploy.yml -p todos-webflux-deploy -l creds.yml
   -v resource_concourse_uri=https://github.com/corbtastik/todos-concourse.git \
   -v resource_release_owner=corbtastik \
   -v resource_release_repository=todos-webflux  
+
+fly -t todos-cicd unpause-pipeline -p todos-webflux-deploy
+  
