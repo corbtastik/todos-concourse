69af5a8d2f76196d91988dba1e90bdc38e1ba3a4
Corbs <corbsmartin@pivotal.io>
2018-12-02 09:38:09 -0600

    detect resource-source-code type

--- ci/detect.sh
+++ ci/detect.sh
@@ -3,28 +3,18 @@ cd resource-source-code
 if [ -e index.html ]
 then
     echo "staticfile" > ../resource-detect-output/detected
-fi
-
-if [ -e pom.xml ]
+elif [ -e pom.xml ]
 then
     echo "maven" > ../resource-detect-output/detected
-fi
-
-if [ -e build.gradle ]
+elif [ -e build.grade ]
 then
     echo "gradle" > ../resource-detect-output/detected
-fi
-
-if [ -e package.json ]
+elif [ -e package.json ]
 then
     echo "node" > ../resource-detect-output/detected
+else
+    echo "null" > ../resource-detect-output/detected
 fi
-
-if [ -e ../resource-detect-output/detected ]
-then
-    echo "detected:"
-    cat ../resource-detect-output/detected
-else 
-    echo "Nothing detected, /detected=false"
-fi
+echo "detected:"
+cat ../resource-detect-output/detected
 cd ..
