08f4dfed32f3d8650f9d3aa725083bfec7bbe0cf
Corbs <corbsmartin@pivotal.io>
2018-12-02 09:57:17 -0600

    adding in detect...again

--- ci/detect.sh
+++ ci/detect.sh
@@ -1,21 +1,21 @@
 #!/bin/sh
 ls -al
-# cd resource-source-code
-# if [ -e index.html ]
-# then
-#     echo "staticfile" > ../resource-detect-output/detected
-# elif [ -e pom.xml ]
-# then
-#     echo "maven" > ../resource-detect-output/detected
-# elif [ -e build.grade ]
-# then
-#     echo "gradle" > ../resource-detect-output/detected
-# elif [ -e package.json ]
-# then
-#     echo "node" > ../resource-detect-output/detected
-# else
-#     echo "null" > ../resource-detect-output/detected
-# fi
-# echo "detected:"
-# cat ../resource-detect-output/detected
-# cd ..
+cd resource-source-code
+if [ -e index.html ]
+then
+    echo "staticfile" > ../resource-detect-output/detected
+elif [ -e pom.xml ]
+then
+    echo "maven" > ../resource-detect-output/detected
+elif [ -e build.grade ]
+then
+    echo "gradle" > ../resource-detect-output/detected
+elif [ -e package.json ]
+then
+    echo "node" > ../resource-detect-output/detected
+else
+    echo "null" > ../resource-detect-output/detected
+fi
+echo "detected:"
+cat ../resource-detect-output/detected
+cd ..
