7a657bbe339d90375991290cd99392b310f546be
Corbs <corbsmartin@pivotal.io>
2018-12-03 15:38:24 -0600

    maintenance bump

--- ci/release_static.sh
+++ ci/release_static.sh
@@ -4,5 +4,7 @@ export VERSION=`cat resource-version/version`
 echo "Packaging Release for $BUILD_NAME version $VERSION" > resource-release-output/release-body
 echo "${BUILD_NAME}" > resource-release-output/release-name
 echo "${VERSION}" > resource-release-output/release-tag
-tar cf resource-release-output/${BUILD_NAME}-${VERSION}-release.tar resource-build-output
-ls -al resource-build-output/
+pushd resource-build-output
+tar cf ${BUILD_NAME}-${VERSION}-release.tar *
+mv ${BUILD_NAME}-${VERSION}-release.tar ../resource-release-output/
+popd
