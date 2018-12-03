2d5ae5f5c4a334566eb5714a3b1105e18d12761a
Corbs <corbsmartin@pivotal.io>
2018-12-03 15:23:30 -0600

    maintenance bump

--- ci/release_static.sh
+++ ci/release_static.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
 env
 export VERSION=`cat resource-version/version`
-export BUILD_NAME='todos-ui'
 echo "Packaging Release for $BUILD_NAME version $VERSION" > resource-release-output/release-body
 echo "${BUILD_NAME}" > resource-release-output/release-name
 echo "${VERSION}" > resource-release-output/release-tag
