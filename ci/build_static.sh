096089c041178b691aece75da9864f435c2b8b2d
Corbs <corbsmartin@pivotal.io>
2018-12-03 12:15:00 -0600

    maintenance bump

--- ci/build_static.sh
+++ ci/build_static.sh
@@ -3,5 +3,5 @@ ls -al
 export DETECTED=`cat resource-unit-output/detected`
 export VERSION=`cat resource-version/version`
 echo "static_build: $DETECTED $VERSION"
-cp -R resource-source-code/* resource-build-output/
-ls -al resource-build-output/
+cp -R resource-source-code/ resource-build-output
+ls -al resource-build-output
