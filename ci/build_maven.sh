791ca8b0ecf580b655e3067d505fc28bc566586e
Corbs <corbsmartin@pivotal.io>
2018-12-03 16:25:49 -0600

    maintenance bump

---
+++ ci/build_maven.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+export MAVEN_OPTS="-Xms2g -Xmx2g"
+export DETECTED=`cat resource-unit-output/detected`
+export VERSION=`cat resource-version/version`
+echo "maven_build: $DETECTED $VERSION"
+cd resource-source-code
+  ./mvnw versions:set -DnewVersion=${VERSION}
+  ./mvnw clean package -Dmaven.test.skip=true
+cd ..
+cp -R resource-source-code/target resource-build-output/.
--- pipeline_build.sh
+++ pipeline_build.sh
@@ -1,6 +1,19 @@
 #!/bin/bash
 
+# fly sp -t todos-cicd -c pipeline-build.yml -p todos-api-build -l creds.yml \
+#   -v resource_concourse_branch=master \
+#   -v resource_concourse_uri=https://github.com/corbtastik/todos-concourse.git \
+#   -v resource_source_code_branch=master \
+#   -v resource_source_code_uri=git@github.com:corbtastik/todos-api.git \
+#   -v resource_version_branch=master \
+#   -v resource_version_bump=patch \
+#   -v resource_version_pre=HOWDY \
+#   -v resource_version_uri=git@github.com:corbtastik/todos-version.git  
+
+# fly -t todos-cicd unpause-pipeline -p todos-api-build
+
 fly sp -t todos-cicd -c pipeline-build.yml -p todos-api-build -l creds.yml \
+  -v build_name=todos-api \
   -v resource_concourse_branch=master \
   -v resource_concourse_uri=https://github.com/corbtastik/todos-concourse.git \
   -v resource_source_code_branch=master \
@@ -8,7 +21,10 @@ fly sp -t todos-cicd -c pipeline-build.yml -p todos-api-build -l creds.yml \
   -v resource_version_branch=master \
   -v resource_version_bump=patch \
   -v resource_version_pre=HOWDY \
-  -v resource_version_uri=git@github.com:corbtastik/todos-version.git  
+  -v resource_version_uri=git@github.com:corbtastik/todos-version.git \
+  -v resource_release_owner=corbtastik \
+  -v resource_release_repository=todos-api \
 
 fly -t todos-cicd unpause-pipeline -p todos-api-build
+  
   
