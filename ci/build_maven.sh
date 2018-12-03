c960b80401f71fac0110549893a77fcb738eabef
Corbs <corbsmartin@pivotal.io>
2018-12-03 16:56:29 -0600

    maintenance bump

--- ci/build_maven.sh
+++ ci/build_maven.sh
@@ -2,9 +2,10 @@
 export MAVEN_OPTS="-Xms2g -Xmx2g"
 export DETECTED=`cat resource-unit-output/detected`
 export VERSION=`cat resource-version/version`
-echo "maven_build: $DETECTED $VERSION"
+export SKIP_UNIT_TESTS=true
+echo "maven_build: ${DETECTED} ${VERSION}"
 cd resource-source-code
 ./mvnw versions:set -DnewVersion=${VERSION}
-./mvnw clean package -Dmaven.test.skip=true
+./mvnw clean package -Dmaven.test.skip=${SKIP_UNIT_TESTS}
 cd target/
 cp -R . ../../resource-build-output/
