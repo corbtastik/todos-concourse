5550b9c3129941026d332c210bd2180e561fa797
Corbs <corbsmartin@pivotal.io>
2018-12-03 11:46:57 -0600

    maintenance bump

--- pipeline_build.sh
+++ pipeline_build.sh
@@ -5,6 +5,10 @@ fly sp -t todos-cicd -c pipeline-build.yml -p todos-api-build -l creds.yml \
   -v resource_concourse_uri=https://github.com/corbtastik/todos-concourse.git \
   -v resource_source_code_branch=master \
   -v resource_source_code_uri=git@github.com:corbtastik/todos-api.git \
+  -v resource_version_branch=master \
+  -v resource_version_bump=patch \
+  -v resource_version_pre=HOWDY \
+  -v resource_version_uri=git@github.com:corbtastik/todos-version.git  
 
 fly -t todos-cicd unpause-pipeline -p todos-api-build
   
