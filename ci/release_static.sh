85b3e1e86cbf332e7f4f581b30130e7e6df2c6bf
Corbs <corbsmartin@pivotal.io>
2018-12-03 14:18:42 -0600

    maintenance bump

---
+++ ci/release_static.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+export VERSION=`cat resource-version/version`
+export BUILD_NAME='todos-ui'
+echo "Packaging Release for $BUILD_NAME version $VERSION" > resource-release-output/release-body
+echo "${BUILD_NAME}" > resource-release-output/release-name
+echo "${VERSION}" > resource-release-output/release-tag
--- pipeline_build_static.sh
+++ pipeline_build_static.sh
@@ -8,7 +8,9 @@ fly sp -t todos-cicd -c pipeline-static-build.yml -p todos-ui-build -l creds.yml
   -v resource_version_branch=master \
   -v resource_version_bump=patch \
   -v resource_version_pre=HOWDY \
-  -v resource_version_uri=git@github.com:corbtastik/todos-version.git  
+  -v resource_version_uri=git@github.com:corbtastik/todos-version.git \
+  -v resource_release_owner=corbtastik \
+  -v resource_release_repository=todos-ui \
 
 fly -t todos-cicd unpause-pipeline -p todos-ui-build
   
