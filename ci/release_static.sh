10e57ffc22f804dec9cebfbfb6176d483bbfad7b
Corbs <corbsmartin@pivotal.io>
2018-12-03 14:59:28 -0600

    maintenance bump

--- ci/release_static.sh
+++ ci/release_static.sh
@@ -4,4 +4,4 @@ export BUILD_NAME='todos-ui'
 echo "Packaging Release for $BUILD_NAME version $VERSION" > resource-release-output/release-body
 echo "${BUILD_NAME}" > resource-release-output/release-name
 echo "${VERSION}" > resource-release-output/release-tag
-tar cf resource-release-output/release-bits.tar resource-build-output
+tar cf resource-release-output/${BUILD_NAME}-${VERSION}-release.tar resource-build-output
--- pipeline_build_static.sh
+++ pipeline_build_static.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 fly sp -t todos-cicd -c pipeline-static-build.yml -p todos-ui-build -l creds.yml \
+  -v build_name=todos-ui \
   -v resource_concourse_branch=master \
   -v resource_concourse_uri=https://github.com/corbtastik/todos-concourse.git \
   -v resource_source_code_branch=master \
