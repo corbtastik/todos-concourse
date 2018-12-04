5d7d5772750a85783246c2333f3d269fbfb43f48
Corbs <corbsmartin@pivotal.io>
2018-12-04 13:42:52 -0600

    maintenance push

--- ci/build_static.sh
+++ ci/build_static.sh
@@ -1,13 +1,8 @@
 #!/bin/sh
-ls -al
 export DETECTED=`cat resource-detect/detected`
 if [ "${DETECTED}" = "static" ]; then
     export VERSION=`cat resource-version/version`
     echo "static_build: $DETECTED $VERSION"
     cd resource-unit
     cp -R . ../resource-build/
-    cd ..
-    cd resource-build
-    ls -al
-    cd ..
 fi
