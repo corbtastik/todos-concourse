518acb2e8d7577cbae98a75ca6caefefdd5c0496
Corbs <corbsmartin@pivotal.io>
2018-12-04 16:22:18 -0600

    maintenance push

--- ci/deploy.sh
+++ ci/deploy.sh
@@ -5,6 +5,9 @@ BUILD_ARTIFACT=`cat resource-release/body | grep -w "build.artifact" | cut -d'='
 BUILD_GROUP=`cat resource-release/body | grep -w "build.group" | cut -d'=' -f2`
 BUILD_VERSION=`cat resource-release/body | grep -w "build.version" | cut -d'=' -f2`
 
-cp resource-release/${BUILD_ARTIFACT}-${BUILD_VERSION}.jar resource-deploy/
-sed -e "s/\${artifact}/${BUILD_ARTIFACT}-${BUILD_VERSION}.jar/" \
-    resource-concourse/ci/manifest/${BUILD_ARTIFACT}-manifest.yml > resource-deploy/manifest.yml
+tar xf resource-release/${BUILD_ARTIFACT}-${BUILD_VERSION}.tar
+ls -al
+ls -al resource-release/
+# cp resource-release/${BUILD_ARTIFACT}-${BUILD_VERSION}.jar resource-deploy/
+# sed -e "s/\${artifact}/${BUILD_ARTIFACT}-${BUILD_VERSION}.jar/" \
+#     resource-concourse/ci/manifest/${BUILD_ARTIFACT}-manifest.yml > resource-deploy/manifest.yml
