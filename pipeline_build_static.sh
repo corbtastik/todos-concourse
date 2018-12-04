0f5ea0af382382c308f377a89edc92e50210e751
Corbs <corbsmartin@pivotal.io>
2018-12-04 13:36:18 -0600

    maintenance push

--- pipeline_build_static.sh
+++ pipeline_build_static.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-fly sp -t todos-cicd -c pipeline-static-build.yml -p todos-ui-build -l creds.yml \
+fly sp -t todos-cicd -c pipeline-build-static.yml -p todos-ui-build -l creds.yml \
   -v build_name=todos-ui \
   -v resource_concourse_branch=master \
   -v resource_concourse_uri=https://github.com/corbtastik/todos-concourse.git \
-  -v resource_source_code_branch=master \
-  -v resource_source_code_uri=git@github.com:corbtastik/todos-ui.git \
+  -v resource_unit_branch=master \
+  -v resource_unit_uri=git@github.com:corbtastik/todos-ui.git \
   -v resource_version_branch=master \
   -v resource_version_bump=patch \
   -v resource_version_pre=HOWDY \
@@ -13,5 +13,4 @@ fly sp -t todos-cicd -c pipeline-static-build.yml -p todos-ui-build -l creds.yml
   -v resource_release_owner=corbtastik \
   -v resource_release_repository=todos-ui \
 
-fly -t todos-cicd unpause-pipeline -p todos-ui-build
-  
+fly -t todos-cicd unpause-pipeline -p todos-ui-build 
