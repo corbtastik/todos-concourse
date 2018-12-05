8f0c6e2512f1df2c374ea897c6efa3519b81b42f
Corbs <corbsmartin@pivotal.io>
2018-12-05 11:45:20 -0600

    maintenance push

--- ci/deploy.sh
+++ ci/deploy.sh
@@ -1,15 +1,13 @@
 #!/bin/bash
 BUILD_VERSION=`cat resource-release/version`
-tar xf resource-release/${BUILD_ARTIFACT}-${BUILD_VERSION}-release.tar
-if [ -e ${BUILD_ARTIFACT}-${BUILD_VERSION}.jar ]; then
-    echo "jar file ${BUILD_ARTIFACT}-${BUILD_VERSION}.jar found"
-    cp ${BUILD_ARTIFACT}-${BUILD_VERSION}.jar resource-deploy/
+tar xf resource-release/${BUILD_ARTIFACT}-${BUILD_VERSION}-release.tar -C resource-deploy/
+if [ -e resource-deploy/${BUILD_ARTIFACT}-${BUILD_VERSION}.jar ]; then
+    echo "jar file resource-deploy/${BUILD_ARTIFACT}-${BUILD_VERSION}.jar found"
     sed -e "s/\${artifact}/${BUILD_ARTIFACT}-${BUILD_VERSION}.jar/" \
         resource-concourse/ci/manifest/${BUILD_ARTIFACT}-manifest.yml > resource-deploy/manifest-cicd.yml    
 fi
 
-if [ -e index.html ]; then
-    echo "index.html found"
-    ls -al
+if [ -e resource-deploy/index.html ]; then
+    echo "resource-deploy/index.html found"
     cat resource-concourse/ci/manifest/${BUILD_ARTIFACT}-manifest.yml > resource-deploy/manifest-cicd.yml
 fi
