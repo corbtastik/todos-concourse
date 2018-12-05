32c84dfb082de35801b3abc05e88ce5a7b611fb6
Corbs <corbsmartin@pivotal.io>
2018-12-05 13:01:14 -0600

    maintenance push

--- pipeline_deploy.sh
+++ pipeline_deploy.sh
@@ -1,12 +1,31 @@
 #!/bin/bash
 
-# fly sp -t todos-cicd -c pipeline-deploy.yml -p todos-webflux-deploy -l creds.yml \
-#   -v resource_concourse_branch=master \
-#   -v resource_concourse_uri=https://github.com/corbtastik/todos-concourse.git \
-#   -v resource_release_owner=corbtastik \
-#   -v resource_release_repository=todos-webflux  
+fly sp -t todos-cicd -c pipeline-deploy.yml -p todos-api-deploy -l creds.yml \
+  -v build_artifact=todos-api \
+  -v resource_concourse_branch=master \
+  -v resource_concourse_uri=https://github.com/corbtastik/todos-concourse.git \
+  -v resource_release_owner=corbtastik \
+  -v resource_release_repository=todos-api
 
-# fly -t todos-cicd unpause-pipeline -p todos-webflux-deploy
+fly -t todos-cicd unpause-pipeline -p todos-api-deploy
+
+fly sp -t todos-cicd -c pipeline-deploy.yml -p todos-webflux-deploy -l creds.yml \
+  -v build_artifact=todos-webflux \
+  -v resource_concourse_branch=master \
+  -v resource_concourse_uri=https://github.com/corbtastik/todos-concourse.git \
+  -v resource_release_owner=corbtastik \
+  -v resource_release_repository=todos-webflux  
+
+fly -t todos-cicd unpause-pipeline -p todos-webflux-deploy
+
+fly sp -t todos-cicd -c pipeline-deploy.yml -p todos-cloud-gateway-deploy -l creds.yml \
+  -v build_artifact=todos-cloud-gateway \
+  -v resource_concourse_branch=master \
+  -v resource_concourse_uri=https://github.com/corbtastik/todos-concourse.git \
+  -v resource_release_owner=corbtastik \
+  -v resource_release_repository=todos-cloud-gateway  
+
+fly -t todos-cicd unpause-pipeline -p todos-cloud-gateway-deploy
 
 fly sp -t todos-cicd -c pipeline-deploy.yml -p todos-ui-deploy -l creds.yml \
   -v build_artifact=todos-ui \
@@ -16,4 +35,3 @@ fly sp -t todos-cicd -c pipeline-deploy.yml -p todos-ui-deploy -l creds.yml \
   -v resource_release_repository=todos-ui  
 
 fly -t todos-cicd unpause-pipeline -p todos-ui-deploy
-  
