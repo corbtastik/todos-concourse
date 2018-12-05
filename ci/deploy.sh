2e9d37165ccc4b778093823686aa6079b258d183
Corbs <corbsmartin@pivotal.io>
2018-12-05 11:31:45 -0600

    maintenance push

--- ci/deploy.sh
+++ ci/deploy.sh
@@ -5,13 +5,10 @@ if [ -e ${BUILD_ARTIFACT}-${BUILD_VERSION}.jar ]; then
     echo "jar file ${BUILD_ARTIFACT}-${BUILD_VERSION}.jar found"
     cp ${BUILD_ARTIFACT}-${BUILD_VERSION}.jar resource-deploy/
     sed -e "s/\${artifact}/${BUILD_ARTIFACT}-${BUILD_VERSION}.jar/" \
-        resource-concourse/ci/manifest/${BUILD_ARTIFACT}-manifest.yml > resource-deploy/manifest.yml    
-
+        resource-concourse/ci/manifest/${BUILD_ARTIFACT}-manifest.yml > resource-deploy/manifest-cicd.yml    
 fi
 
 if [ -e index.html ]; then
     echo "index.html found"
-    cp -R . resource-deploy/
-    cp ${BUILD_ARTIFACT}-${BUILD_VERSION}.jar resource-deploy/
-    cat resource-concourse/ci/manifest/${BUILD_ARTIFACT}-manifest.yml > resource-deploy/manifest.yml
+    cat resource-concourse/ci/manifest/${BUILD_ARTIFACT}-manifest.yml > resource-deploy/manifest-cicd.yml
 fi
