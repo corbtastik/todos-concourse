9e9190ab2f6eaaf0bae5a0d0de6c5c0a00793958
Corbs <corbsmartin@pivotal.io>
2018-12-12 14:37:09 -0600

    deploy nodejs to cf

--- pipeline_deploy.sh
+++ pipeline_deploy.sh
@@ -1,37 +1,48 @@
 #!/bin/bash
 
-fly sp -t todos-cicd -c pipeline-deploy.yml -p todos-api-deploy -l creds.yml \
-  -v build_artifact=todos-api \
+fly sp -t todos-cicd -c pipeline-deploy.yml -p todos-nodejs-deploy -l creds.yml \
+  -v build_artifact=todos-nodejs \
   -v resource_concourse_branch=master \
   -v resource_concourse_uri=https://github.com/corbtastik/todos-concourse.git \
   -v resource_release_owner=corbtastik \
-  -v resource_release_repository=todos-api
+  -v resource_release_repository=todos-nodejs
 
-fly -t todos-cicd unpause-pipeline -p todos-api-deploy
+fly -t todos-cicd unpause-pipeline -p todos-nodejs-deploy
 
-fly sp -t todos-cicd -c pipeline-deploy.yml -p todos-webflux-deploy -l creds.yml \
-  -v build_artifact=todos-webflux \
-  -v resource_concourse_branch=master \
-  -v resource_concourse_uri=https://github.com/corbtastik/todos-concourse.git \
-  -v resource_release_owner=corbtastik \
-  -v resource_release_repository=todos-webflux  
+# fly -t todos-cicd unpause-pipeline -p todos-api-deploy
 
-fly -t todos-cicd unpause-pipeline -p todos-webflux-deploy
+# fly sp -t todos-cicd -c pipeline-deploy.yml -p todos-api-deploy -l creds.yml \
+#   -v build_artifact=todos-api \
+#   -v resource_concourse_branch=master \
+#   -v resource_concourse_uri=https://github.com/corbtastik/todos-concourse.git \
+#   -v resource_release_owner=corbtastik \
+#   -v resource_release_repository=todos-api
 
-fly sp -t todos-cicd -c pipeline-deploy.yml -p todos-cloud-gateway-deploy -l creds.yml \
-  -v build_artifact=todos-cloud-gateway \
-  -v resource_concourse_branch=master \
-  -v resource_concourse_uri=https://github.com/corbtastik/todos-concourse.git \
-  -v resource_release_owner=corbtastik \
-  -v resource_release_repository=todos-cloud-gateway  
+# fly -t todos-cicd unpause-pipeline -p todos-api-deploy
 
-fly -t todos-cicd unpause-pipeline -p todos-cloud-gateway-deploy
+# fly sp -t todos-cicd -c pipeline-deploy.yml -p todos-webflux-deploy -l creds.yml \
+#   -v build_artifact=todos-webflux \
+#   -v resource_concourse_branch=master \
+#   -v resource_concourse_uri=https://github.com/corbtastik/todos-concourse.git \
+#   -v resource_release_owner=corbtastik \
+#   -v resource_release_repository=todos-webflux  
 
-fly sp -t todos-cicd -c pipeline-deploy.yml -p todos-ui-deploy -l creds.yml \
-  -v build_artifact=todos-ui \
-  -v resource_concourse_branch=master \
-  -v resource_concourse_uri=https://github.com/corbtastik/todos-concourse.git \
-  -v resource_release_owner=corbtastik \
-  -v resource_release_repository=todos-ui  
+# fly -t todos-cicd unpause-pipeline -p todos-webflux-deploy
 
-fly -t todos-cicd unpause-pipeline -p todos-ui-deploy
+# fly sp -t todos-cicd -c pipeline-deploy.yml -p todos-cloud-gateway-deploy -l creds.yml \
+#   -v build_artifact=todos-cloud-gateway \
+#   -v resource_concourse_branch=master \
+#   -v resource_concourse_uri=https://github.com/corbtastik/todos-concourse.git \
+#   -v resource_release_owner=corbtastik \
+#   -v resource_release_repository=todos-cloud-gateway  
+
+# fly -t todos-cicd unpause-pipeline -p todos-cloud-gateway-deploy
+
+# fly sp -t todos-cicd -c pipeline-deploy.yml -p todos-ui-deploy -l creds.yml \
+#   -v build_artifact=todos-ui \
+#   -v resource_concourse_branch=master \
+#   -v resource_concourse_uri=https://github.com/corbtastik/todos-concourse.git \
+#   -v resource_release_owner=corbtastik \
+#   -v resource_release_repository=todos-ui  
+
+# fly -t todos-cicd unpause-pipeline -p todos-ui-deploy
