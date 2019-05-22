361ce3a4065d29294ca6c1db0af369a1a9f97fd2
corbs <corbsmartin@gmail.com>
2019-05-22 08:37:35 -0500

    adding webui manifest

--- pipeline_build.sh
+++ pipeline_build.sh
@@ -1,71 +1,30 @@
 #!/bin/bash
-fly sp -t todos-cicd -c pipeline-build-maven.yml -p todos-api-build -l creds.yml \
-  -v build_name=todos-api \
+if [ -z "$1" ] 
+then
+    echo "Please enter an app (for example todos-api)"
+    exit 1
+fi
+APP=$1
+# Set these variables to suit your needs
+PIPELINE_NAME=${APP}-build  
+BUILD_NAME=${APP}
+UNIT_BRANCH=master
+UNIT_URI=git@github.com:corbtastik/${APP}.git
+VERSION_BRANCH=master
+VERSION_URI=git@github.com:corbtastik/todos-version.git
+VERSION_BUMP=patch
+VERSION_TAG=HOWDY
+RELEASE_REPOSITORY=${APP}
+RELEASE_OWNER=corbtastik
+fly sp -t ci -c pipeline-build-maven.yml -p ${PIPELINE_NAME} -l creds.yml \
+  -v build_name=${BUILD_NAME} \
   -v resource_concourse_branch=master \
   -v resource_concourse_uri=https://github.com/corbtastik/todos-concourse.git \
-  -v resource_unit_branch=master \
-  -v resource_unit_uri=git@github.com:corbtastik/todos-api.git \
-  -v resource_version_branch=master \
-  -v resource_version_bump=patch \
-  -v resource_version_pre=HOWDY \
-  -v resource_version_uri=git@github.com:corbtastik/todos-version.git \
-  -v resource_release_owner=corbtastik \
-  -v resource_release_repository=todos-api \
-
-fly sp -t todos-cicd -c pipeline-build-maven.yml -p todos-webflux-build -l creds.yml \
-  -v build_name=todos-webflux \
-  -v resource_concourse_branch=master \
-  -v resource_concourse_uri=https://github.com/corbtastik/todos-concourse.git \
-  -v resource_unit_branch=master \
-  -v resource_unit_uri=git@github.com:corbtastik/todos-webflux.git \
-  -v resource_version_branch=master \
-  -v resource_version_bump=patch \
-  -v resource_version_pre=HOWDY \
-  -v resource_version_uri=git@github.com:corbtastik/todos-version.git \
-  -v resource_release_owner=corbtastik \
-  -v resource_release_repository=todos-webflux \
-
-fly sp -t todos-cicd -c pipeline-build-maven.yml -p todos-cloud-gateway-build -l creds.yml \
-  -v build_name=todos-cloud-gateway \
-  -v resource_concourse_branch=master \
-  -v resource_concourse_uri=https://github.com/corbtastik/todos-concourse.git \
-  -v resource_unit_branch=master \
-  -v resource_unit_uri=git@github.com:corbtastik/todos-cloud-gateway.git \
-  -v resource_version_branch=master \
-  -v resource_version_bump=patch \
-  -v resource_version_pre=HOWDY \
-  -v resource_version_uri=git@github.com:corbtastik/todos-version.git \
-  -v resource_release_owner=corbtastik \
-  -v resource_release_repository=todos-cloud-gateway \
-
-fly sp -t todos-cicd -c pipeline-build-static.yml -p todos-ui-build -l creds.yml \
-  -v build_name=todos-ui \
-  -v resource_concourse_branch=master \
-  -v resource_concourse_uri=https://github.com/corbtastik/todos-concourse.git \
-  -v resource_unit_branch=master \
-  -v resource_unit_uri=git@github.com:corbtastik/todos-ui.git \
-  -v resource_version_branch=master \
-  -v resource_version_bump=patch \
-  -v resource_version_pre=HOWDY \
-  -v resource_version_uri=git@github.com:corbtastik/todos-version.git \
-  -v resource_release_owner=corbtastik \
-  -v resource_release_repository=todos-ui \
-
-fly sp -t todos-cicd -c pipeline-build-nodejs.yml -p todos-nodejs-build -l creds.yml \
-  -v build_name=todos-nodejs \
-  -v resource_concourse_branch=master \
-  -v resource_concourse_uri=https://github.com/corbtastik/todos-concourse.git \
-  -v resource_unit_branch=master \
-  -v resource_unit_uri=git@github.com:corbtastik/todos-nodejs.git \
-  -v resource_version_branch=master \
-  -v resource_version_bump=patch \
-  -v resource_version_pre=HOWDY \
-  -v resource_version_uri=git@github.com:corbtastik/todos-version.git \
-  -v resource_release_owner=corbtastik \
-  -v resource_release_repository=todos-nodejs \  
-
-fly -t todos-cicd unpause-pipeline -p todos-api-build
-fly -t todos-cicd unpause-pipeline -p todos-webflux-build
-fly -t todos-cicd unpause-pipeline -p todos-cloud-gateway-build
-fly -t todos-cicd unpause-pipeline -p todos-ui-build   
-fly -t todos-cicd unpause-pipeline -p todos-nodejs-build   
+  -v resource_unit_branch=${UNIT_BRANCH} \
+  -v resource_unit_uri=${UNIT_URI} \
+  -v resource_version_branch=${VERSION_BRANCH} \
+  -v resource_version_bump=${VERSION_BUMP} \
+  -v resource_version_pre=${VERSION_TAG} \
+  -v resource_version_uri=${VERSION_URI} \
+  -v resource_release_owner=${RELEASE_OWNER} \
+  -v resource_release_repository=${RELEASE_REPOSITORY}
--- pipeline_deploy.sh
+++ pipeline_deploy.sh
@@ -1,41 +1,19 @@
 #!/bin/bash
-fly sp -t todos-cicd -c pipeline-deploy.yml -p todos-api-deploy -l creds.yml \
-  -v build_artifact=todos-api \
+if [ -z "$1" ] 
+then
+    echo "Please enter an app (for example todos-api)"
+    exit 1
+fi
+APP=$1
+# Set these variables to suit your needs
+PIPELINE_NAME=${APP}-deploy  
+BUILD_ARTIFACT=${APP}
+RELEASE_REPOSITORY=${APP}
+RELEASE_OWNER=corbtastik
+
+fly sp -t ci -c pipeline-deploy.yml -p ${PIPELINE_NAME} -l creds.yml \
+  -v build_artifact=${BUILD_ARTIFACT} \
   -v resource_concourse_branch=master \
   -v resource_concourse_uri=https://github.com/corbtastik/todos-concourse.git \
-  -v resource_release_owner=corbtastik \
-  -v resource_release_repository=todos-api
-
-fly sp -t todos-cicd -c pipeline-deploy.yml -p todos-webflux-deploy -l creds.yml \
-  -v build_artifact=todos-webflux \
-  -v resource_concourse_branch=master \
-  -v resource_concourse_uri=https://github.com/corbtastik/todos-concourse.git \
-  -v resource_release_owner=corbtastik \
-  -v resource_release_repository=todos-webflux  
-
-fly sp -t todos-cicd -c pipeline-deploy.yml -p todos-cloud-gateway-deploy -l creds.yml \
-  -v build_artifact=todos-cloud-gateway \
-  -v resource_concourse_branch=master \
-  -v resource_concourse_uri=https://github.com/corbtastik/todos-concourse.git \
-  -v resource_release_owner=corbtastik \
-  -v resource_release_repository=todos-cloud-gateway  
-
-fly sp -t todos-cicd -c pipeline-deploy.yml -p todos-ui-deploy -l creds.yml \
-  -v build_artifact=todos-ui \
-  -v resource_concourse_branch=master \
-  -v resource_concourse_uri=https://github.com/corbtastik/todos-concourse.git \
-  -v resource_release_owner=corbtastik \
-  -v resource_release_repository=todos-ui  
-
-fly sp -t todos-cicd -c pipeline-deploy.yml -p todos-nodejs-deploy -l creds.yml \
-  -v build_artifact=todos-nodejs \
-  -v resource_concourse_branch=master \
-  -v resource_concourse_uri=https://github.com/corbtastik/todos-concourse.git \
-  -v resource_release_owner=corbtastik \
-  -v resource_release_repository=todos-nodejs  
-
-fly -t todos-cicd unpause-pipeline -p todos-api-deploy
-fly -t todos-cicd unpause-pipeline -p todos-webflux-deploy
-fly -t todos-cicd unpause-pipeline -p todos-cloud-gateway-deploy
-fly -t todos-cicd unpause-pipeline -p todos-ui-deploy
-fly -t todos-cicd unpause-pipeline -p todos-nodejs-deploy
+  -v resource_release_owner=${RELEASE_OWNER} \
+  -v resource_release_repository=${RELEASE_REPOSITORY}
